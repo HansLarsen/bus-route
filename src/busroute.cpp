6b42626f686fafe260dc7e505861a5b283c1beae
hax <hans.larsen1994@gmail.com>
2018-12-02 14:28:40 +0100

    Removed Interactive marker

--- src/busroute.cpp
+++ src/busroute.cpp
@@ -15,15 +15,6 @@
 using namespace std;
 using namespace ros;
 using namespace geometry_msgs;
-bool trueOn = false;
-
-void _processFeedback(const visualization_msgs::InteractiveMarkerFeedbackConstPtr &feedback )
-{
-    ROS_INFO_STREAM( feedback->marker_name << " is now at "
-    << feedback->pose.position.x << ", " << feedback->pose.position.y
-    << ", " << feedback->pose.position.z );
-    trueOn = true;
-}
 
 class Route
 {
@@ -132,7 +123,7 @@ private:
         _sort_routes_next_closest();
         _send_markers();
 
-        if (trueOn == true)
+        if (stops_initialized > 3)
         {
             _send_goal(points[0]);
         }
@@ -224,59 +215,6 @@ int main(int argc, char *argv[])
     ros::init(argc, argv, "busroute");
 
     Route r;
-
-        /*
-    #############################################################################################
-    ##                                      InterActive Marker                                 ##
-    #############################################################################################
-    */
-
-    // create an interactive marker server on the topic namespace simple_marker
-    interactive_markers::InteractiveMarkerServer server("simple_marker");
-
-    // create an interactive marker for our server
-    visualization_msgs::InteractiveMarker int_marker;
-    int_marker.header.frame_id = "base_link";
-    int_marker.header.stamp=ros::Time::now();
-    int_marker.name = "my_marker";
-    int_marker.description = "On Button";
-
-    // create a grey box marker
-    visualization_msgs::Marker box_marker;
-    box_marker.type = visualization_msgs::Marker::CUBE;
-    box_marker.scale.x = 0.45;
-    box_marker.scale.y = 0.45;
-    box_marker.scale.z = 0.45;
-    box_marker.color.r = 0.5;
-    box_marker.color.g = 0.5;
-    box_marker.color.b = 0.5;
-    box_marker.color.a = 1.0;
-
-    // create a non-interactive control which contains the box
-    visualization_msgs::InteractiveMarkerControl box_control;
-    box_control.always_visible = true;
-    box_control.markers.push_back( box_marker );
-
-    // add the control to the interactive marker
-    int_marker.controls.push_back( box_control );
-
-    // create a control whice interactive markerh will move the box
-    // this control does not  int_marker.controls.push_back(rotate_control); contain any markers,
-    // which will cause RViz to insert two arrows
-    visualization_msgs::InteractiveMarkerControl rotate_control;
-    rotate_control.name = "move_x";  // tell the server to call processFeedback() when feedback arrives for itove_x";
-    rotate_control.interaction_mode =
-        visualization_msgs::InteractiveMarkerControl::MOVE_AXIS;
-    // 'commit' changes and send to all clients
-    // add the control to thserver.applyChanges();e interactive marker
-    int_marker.controls.push_back(rotate_control);
-
-    // add the interactive marker to our collection &
-    // tell the server to call processFeedback() when feedback arrives for it
-    server.insert(int_marker, &_processFeedback );
-
-    // 'commit' changes and send to all clients
-    server.applyChanges();
 
     ros::spin();
     return 0;
